409413d1225d488866d1c8c85dbef8a439383ee0
Rodrigo de Sousa <rodrigo.ross.comp@gmail.com>
2022-12-26 21:11:38 -0400

    aula 7 - adiciona dados para envio de email

---
+++ app/Listeners/ProductUpdatedListener.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Listeners;
+
+class ProductUpdatedListener
+{
+	// public function handle(ProductUpdatedEvent $event)
+	// {
+	//     \Cache::forget('products_frontend');
+	//     \Cache::forget('products_backend');
+	// }
+}
